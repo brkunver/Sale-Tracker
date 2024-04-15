183655d58a1d391a0e7b54acd6d80ee119c73c9d
Code Therapy <166712460+codethrpy@users.noreply.github.com>
2024-04-15 11:37:38 +0300

    delete line

--- sale-tracker-frontend/src/App.tsx
+++ sale-tracker-frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import LoginPage from "./pages/LoginPage"
 import { authLoader } from "./utils/authLoader"
