9e39848c4c769fba77e57373d02ea54a523bdc9c
Code Therapy <166712460+codethrpy@users.noreply.github.com>
2024-04-15 11:38:13 +0300

    add reload functionality

--- sale-tracker-frontend/src/pages/DashboardPage.tsx
+++ sale-tracker-frontend/src/pages/DashboardPage.tsx
@@ -1,16 +1,34 @@
 import SideBar from "@/components/SideBar"
 import Products from "@/components/products/Products"
 import Sales from "@/components/sales/Sales"
+import { RefreshCcw } from "lucide-react"
+
+import { useState } from "react"
 
 function DashboardPage() {
+  const [reloads, setReloads] = useState({
+    reloadProducts: () => null,
+    reloadSales: () => null,
+  })
+
+  async function reloadHandler() {
+    reloads.reloadProducts()
+    reloads.reloadSales()
+    console.log("Reloading")
+  }
+
   return (
     <div className="flex">
       <SideBar />
       <main className="flex flex-col items-center text-center mx-auto">
         <h1 className="text-3xl lg:py-8">Dashboard</h1>
+        <button onClick={reloadHandler} className="flex justify-center items-center text-lg font-semibold rounded bg-gray-200 px-2 py-1 gap-2 mb-4 hover:outline active:scale-90">
+          <span>Reload All</span>
+          <RefreshCcw size={24} className="text-green-700"/>
+        </button>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 px-2">
-          <Products className=""  />
-          <Sales className="" />
+          <Products className="" setRefetch={setReloads} />
+          <Sales className="" setRefetch={setReloads} />
         </div>
       </main>
     </div>
