da0c54e6c1fd4f4f29e41fbd6fe3ea8839e58fc3
BurakHan Unver <burakhanunver@gmail.com>
2024-04-16 10:44:16 +0300

    Create addProduct card

---
+++ sale-tracker-frontend/src/components/products/AddProduct.tsx
@@ -0,0 +1,41 @@
+import { AddNewProduct } from "@/utils/productApiCalls"
+import { Button } from "../ui/button"
+import { Input } from "../ui/input"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+
+export default function AddProduct() {
+  const queryClient = useQueryClient()
+  const mutation = useMutation({
+    mutationFn: async (formData: FormData) => {
+      return await AddNewProduct(formData)
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["products"] })
+    },
+  })
+
+  function formSubmitHandler(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    let formData = new FormData(event.currentTarget)
+    mutation.mutate(formData)
+  }
+
+  return (
+    <form onSubmit={formSubmitHandler} className="mx-auto flex flex-col px-8 gap-2 border rounded py-10">
+      <h2 className="text-center font-semibold text-lg mb-2">Add New Product</h2>
+
+      <div className="grid lg:grid-cols-2">
+        <span className="font-semibold">Select a jpg image or leave empty</span>
+        <Input type="file" name="FormFile" accept="image/jpeg" />
+      </div>
+      <Input type="text" required aria-required name="Name" placeholder="Product Name" />
+      <Input type="text" required aria-required name="Description" placeholder="Description" />
+
+      <Input type="text" required aria-required pattern="^\d{1,10}(,\d{1,2})?$" name="Price" placeholder="Price" />
+      
+      <Button className="w-full mx-auto px-2" type="submit">
+        Add Product
+      </Button>
+    </form>
+  )
+}
