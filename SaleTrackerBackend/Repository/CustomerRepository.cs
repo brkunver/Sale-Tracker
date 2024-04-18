1a3a8a286b5d428c659db033f133913b0c502ab4
burakhanunver <burakhanunver@gmail.com>
2024-04-18 12:01:26 +0300

    create customer repo

--- SaleTrackerBackend/Repository/CustomerRepository.cs
+++ SaleTrackerBackend/Repository/CustomerRepository.cs
@@ -3,7 +3,6 @@ namespace SaleTrackerBackend.Repository;
 using Microsoft.EntityFrameworkCore;
 using SaleTrackerBackend.Data;
 using SaleTrackerBackend.Models;
-using SaleTrackerBackend.Models.Dto;
 
 public class CustomerRepository
 {
@@ -25,8 +24,99 @@ public class CustomerRepository
     }
   }
 
-  public async Task<Customer?> GetCustomerByIdAsync(int id)
+  public async Task<Customer?> GetByIdAsync(int id)
   {
-    return await db.Customers.FirstOrDefaultAsync(customer => customer.CustomerId == id);
+    try
+    {
+      return await db.Customers.Where(c => c.CustomerId == id && !c.IsDeleted).FirstOrDefaultAsync();
+    }
+    catch (Exception)
+    {
+      throw new Exception("Cannot get customer");
+    }
+  }
+
+  public async Task<List<Customer>?> GetAllAsync(int page, int count)
+  {
+    try
+    {
+      var customers = db.Customers.Where(c => !c.IsDeleted).Skip((page - 1) * count).Take(count).OrderBy(c => c.CustomerId);
+      return await customers.ToListAsync();
+    }
+    catch (Exception)
+    {
+      return null;
+    }
+  }
+
+
+  public async Task CreateAsync(Customer customer)
+  {
+    try
+    {
+      await db.Customers.AddAsync(customer);
+      await SaveAsync();
+    }
+    catch (Exception ex)
+    {
+      throw new Exception("Failed to create customer", ex);
+    }
+  }
+
+  public async Task UpdateAsync(Customer customer)
+  {
+    try
+    {
+      var currentCustomer = await db.Customers.FindAsync(customer.CustomerId);
+      if (currentCustomer is null)
+      {
+        throw new Exception("Customer not found");
+      }
+      currentCustomer.Name = customer.Name;
+      currentCustomer.Phone = customer.Phone;
+      currentCustomer.Address = customer.Address;
+      currentCustomer.UpdatedOn = DateTime.Now;
+      await SaveAsync();
+    }
+    catch (Exception ex)
+    {
+      throw new Exception("Failed to update customer", ex);
+    }
+  }
+
+  public async Task MarkDeleted(int id)
+  {
+    try
+    {
+      var customer = await db.Customers.FindAsync(id);
+      if (customer is null)
+      {
+        throw new Exception("Customer not found");
+      }
+      customer.IsDeleted = true;
+      await SaveAsync();
+    }
+    catch (Exception ex)
+    {
+      throw new Exception("Failed to delete customer", ex);
+    }
+  }
+
+  public async Task DeletePermanently(int id)
+  {
+    try
+    {
+      var customer = await db.Customers.FindAsync(id);
+      if (customer is null)
+      {
+        throw new Exception("Customer not found");
+      }
+      db.Customers.Remove(customer);
+      await SaveAsync();
+    }
+    catch (Exception ex)
+    {
+      throw new Exception("Failed to delete customer", ex);
+    }
   }
 }
