68c58e4c9dd2abf64f9edaee109ca4823fac05fe
burakhanunver <burakhanunver@gmail.com>
2024-04-18 12:20:26 +0300

    add get by id endpoint

--- SaleTrackerBackend/Controllers/CustomerController.cs
+++ SaleTrackerBackend/Controllers/CustomerController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using SaleTrackerBackend.Models;
+using SaleTrackerBackend.Models.Dto;
+using SaleTrackerBackend.Repository;
 
 namespace SaleTrackerBackend.Controllers;
 
@@ -6,5 +9,30 @@ namespace SaleTrackerBackend.Controllers;
 [Route("api/customer")]
 public class CustomerController : ControllerBase
 {
+
+  private readonly CustomerRepository customerRepo;
+  public CustomerController(CustomerRepository customerRepo)
+  {
+    this.customerRepo = customerRepo;
+  }
+
+  [HttpGet("{id}")]
+  public async Task<ActionResult<ResponseDto<Customer>>> GetById([FromRoute] int id)
+  {
+    try
+    {
+      var currentCustomer = await customerRepo.GetByIdAsync(id);
+      if (currentCustomer is null)
+      {
+        return NotFound(new ResponseDto<Customer> { Success = false, Message = "Not found" });
+      }
+      return Ok(new ResponseDto<Customer> { Data = currentCustomer });
+    }
+    catch (System.Exception)
+    {
+
+      return BadRequest(new ResponseDto<Customer> { Success = false, Message = "Error" });
+    }
+  }
 
 }
