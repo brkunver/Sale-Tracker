d212cf406af24ae9744d241c4874e71b22d087e8
burakhanunver <burakhanunver@gmail.com>
2024-04-19 11:31:03 +0300

    add validation

--- SaleTrackerBackend/Models/Customer.cs
+++ SaleTrackerBackend/Models/Customer.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SaleTrackerBackend.Models;
 
 public class Customer
 {
     public int CustomerId { get; set; }
+    [MaxLength(100)]
     public string Name { get; set; } = "";
+    [RegularExpression("^[0-9]*$", ErrorMessage = "Invalid phone number"), MaxLength(20)]
     public string Phone { get; set; } = "";
+    [MaxLength(500)]
     public string Address { get; set; } = "";
     public DateTime CreatedOn { get; set; } = DateTime.Now;
     public DateTime UpdatedOn { get; set; } = DateTime.Now;
