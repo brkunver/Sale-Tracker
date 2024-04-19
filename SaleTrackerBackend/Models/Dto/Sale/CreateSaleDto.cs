683c9629abb4a438e54af0e58c0a4ae95741f4e1
burakhanunver <burakhanunver@gmail.com>
2024-04-19 13:30:43 +0300

    add new fields to dto

--- SaleTrackerBackend/Models/Dto/Sale/CreateSaleDto.cs
+++ SaleTrackerBackend/Models/Dto/Sale/CreateSaleDto.cs
@@ -7,4 +7,10 @@ public class CreateSaleDto
     [Required]
     public int? ProductId { get; set; }
     public DateTime? SaledOn { get; set; } = DateTime.Now;
+    [Required]
+    public int? Quantity { get; set; }
+    [Required]
+    public decimal? Total { get; set; }
+    [Required]
+    public int CustomerId { get; set; }
 }
