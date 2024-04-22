9393f593a54c324e044f1690f550b3054ba89e56
burakhanunver <burakhanunver@gmail.com>
2024-04-22 12:35:47 +0300

    move namespace line, change nullable int to non nullable

--- SaleTrackerBackend/Models/Sale.cs
+++ SaleTrackerBackend/Models/Sale.cs
@@ -1,16 +1,16 @@
+namespace SaleTrackerBackend.Models;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace SaleTrackerBackend.Models;
 
 public class Sale
 {
     public int SaleId { get; set; }
     public DateTime SaledOn { get; set; } = DateTime.Now;
    
-    public int? ProductId { get; set; }
+    public int ProductId { get; set; }
     public Product Product { get; set; } = new();
 
-    public int? CustomerId { get; set; }
+    public int CustomerId { get; set; }
     public Customer Customer { get; set; } = new();
     
     public int Quantity { get; set; }
