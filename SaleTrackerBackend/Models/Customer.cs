f3e547b0a14af8294e62fb37cf64ec87b848c740
burakhanunver <burakhanunver@gmail.com>
2024-04-26 11:03:19 +0300

    scaffold db

---
+++ SaleTrackerBackend/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿namespace SaleTrackerBackend.Models;
+
+public partial class Customer
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Phone { get; set; }
+
+    public string? Address { get; set; }
+
+    public DateTime? CreatedOn { get; set; }
+
+    public DateTime? UpdatedOn { get; set; }
+
+    public bool? IsDeleted { get; set; }
+
+    public virtual ICollection<Sale> Sales { get; set; } = new List<Sale>();
+}
---
+++ SaleTrackerBackend/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿namespace SaleTrackerBackend.Models;
+
+public partial class Product
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public decimal Price { get; set; }
+
+    public DateTime? CreatedOn { get; set; }
+
+    public DateTime? UpdatedOn { get; set; }
+
+    public string? ImageUrl { get; set; }
+
+    public bool? IsDeleted { get; set; }
+
+    public virtual ICollection<ProductSale> ProductSales { get; set; } = new List<ProductSale>();
+}
