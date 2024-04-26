26fcf19a5256cdf183b272cecc569f43bcad9715
burakhanunver <burakhanunver@gmail.com>
2024-04-26 13:42:16 +0300

    create migration

---
+++ SaleTrackerBackend/Migrations/20240426080906_AddDefaultValues.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SaleTrackerBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDefaultValues : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK__Sales__CustomerI__44FF419A",
+                table: "Sales");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "SaledOn",
+                table: "Sales",
+                type: "datetime",
+                nullable: false,
+                defaultValueSql: "(getdate())",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldNullable: true,
+                oldDefaultValueSql: "(getdate())");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CustomerId",
+                table: "Sales",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedOn",
+                table: "Products",
+                type: "datetime",
+                nullable: false,
+                defaultValueSql: "(getdate())",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldNullable: true,
+                oldDefaultValueSql: "(getdate())");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDeleted",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true,
+                oldDefaultValue: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedOn",
+                table: "Products",
+                type: "datetime",
+                nullable: false,
+                defaultValueSql: "(getdate())",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldNullable: true,
+                oldDefaultValueSql: "(getdate())");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Customers",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDeleted",
+                table: "Customers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true,
+                oldDefaultValue: false);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedOn",
+                table: "Customers",
+                type: "datetime",
+                nullable: false,
+                defaultValueSql: "(getdate())",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldNullable: true,
+                oldDefaultValueSql: "(getdate())");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Customers",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK__Sales__CustomerI__44FF419A",
+                table: "Sales",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK__Sales__CustomerI__44FF419A",
+                table: "Sales");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "SaledOn",
+                table: "Sales",
+                type: "datetime",
+                nullable: true,
+                defaultValueSql: "(getdate())",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValueSql: "(getdate())");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CustomerId",
+                table: "Sales",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedOn",
+                table: "Products",
+                type: "datetime",
+                nullable: true,
+                defaultValueSql: "(getdate())",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValueSql: "(getdate())");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDeleted",
+                table: "Products",
+                type: "bit",
+                nullable: true,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedOn",
+                table: "Products",
+                type: "datetime",
+                nullable: true,
+                defaultValueSql: "(getdate())",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValueSql: "(getdate())");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Customers",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDeleted",
+                table: "Customers",
+                type: "bit",
+                nullable: true,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: false);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedOn",
+                table: "Customers",
+                type: "datetime",
+                nullable: true,
+                defaultValueSql: "(getdate())",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValueSql: "(getdate())");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Customers",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK__Sales__CustomerI__44FF419A",
+                table: "Sales",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id");
+        }
+    }
+}
