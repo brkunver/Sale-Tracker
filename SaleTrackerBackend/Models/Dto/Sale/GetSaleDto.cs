03cab016ed815304d54493a9b154f01207e323b0
burakhanunver <burakhanunver@gmail.com>
2024-04-26 10:38:31 +0300

    delete file

--- SaleTrackerBackend/Models/Dto/Sale/GetSaleDto.cs
+++
@@ -1,8 +0,0 @@
-namespace SaleTrackerBackend.Models.Dto;
-
-public class GetSaleDto
-{
-    public int SaleId { get; set; }
-    public DateTime SaledOn { get; set; } = DateTime.Now;
-    public int ProductId { get; set; }
-}
