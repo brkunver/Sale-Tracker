128bfc0b7ba21278f151e049d46b464bdc53f6ea
burakhanunver <burakhanunver@gmail.com>
2024-04-26 13:43:33 +0300

    refactor model

--- SaleTrackerBackend/Models/Customer.cs
+++ SaleTrackerBackend/Models/Customer.cs
@@ -4,17 +4,17 @@ public partial class Customer
 {
     public Guid Id { get; set; }
 
-    public string Name { get; set; } = null!;
+    public string Name { get; set; } = "";
 
-    public string? Phone { get; set; }
+    public string Phone { get; set; } = "";
 
-    public string? Address { get; set; }
+    public string Address { get; set; } = "";
 
-    public DateTime? CreatedOn { get; set; }
+    public DateTime CreatedOn { get; set; } = DateTime.Now;
 
-    public DateTime? UpdatedOn { get; set; }
+    public DateTime? UpdatedOn { get; set; } = DateTime.Now;
 
-    public bool? IsDeleted { get; set; }
+    public bool IsDeleted { get; set; } = false;
 
     public virtual ICollection<Sale> Sales { get; set; } = new List<Sale>();
 }
