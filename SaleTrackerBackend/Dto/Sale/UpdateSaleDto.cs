10d71b065ab806fe4fb66f385812a99a573c6ec2
burakhanunver <burakhanunver@gmail.com>
2024-04-29 11:00:36 +0300

    create saleDtos

---
+++ SaleTrackerBackend/Dto/Sale/UpdateSaleDto.cs
@@ -0,0 +1,8 @@
+namespace SaleTrackerBackend.Models;
+
+public partial class UpdateSaleDto
+{
+  public DateTime SaledOn { get; set; }
+  public Guid CustomerId { get; set; }
+  public decimal Total { get; set; }
+}
