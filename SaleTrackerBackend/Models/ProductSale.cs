93025d6f4e88e64a87b2918abe057c54b1ba0bfa
burakhanunver <burakhanunver@gmail.com>
2024-04-30 14:35:44 +0300

    add jsonignore attribute

--- SaleTrackerBackend/Models/ProductSale.cs
+++ SaleTrackerBackend/Models/ProductSale.cs
@@ -10,7 +10,9 @@ public partial class ProductSale
 
     public decimal SaledPrice { get; set; }
 
+    [System.Text.Json.Serialization.JsonIgnore]
     public virtual Product Product { get; set; } = null!;
 
+    [System.Text.Json.Serialization.JsonIgnore]
     public virtual Sale Sale { get; set; } = null!;
 }
