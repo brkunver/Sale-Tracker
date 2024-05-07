1ad20378229c6929c14ce13064f95cdf559091b1
burakhanunver <burakhanunver@gmail.com>
2024-05-07 10:52:41 +0300

    add querynames enum

--- sale-tracker-frontend/src/utils/saleApiCalls.ts
+++ sale-tracker-frontend/src/utils/saleApiCalls.ts
@@ -1,5 +1,10 @@
 import type { Sale, SaleData } from "@/types/saleTypes"
 
+enum queryNames {
+  page = "page",
+  count = "count",
+}
+
 function getTokenHeader() {
   let bearerToken = localStorage.getItem("token")
   let headers = new Headers()
@@ -11,10 +16,10 @@ export async function getAllSales(page?: number, count?: number) {
   let url = import.meta.env.VITE_API_URL + "/api/sale"
   let params = new URLSearchParams()
   if (page) {
-    params.append("page", page.toString())
+    params.append(queryNames.page, page.toString())
   }
   if (count) {
-    params.append("count", count.toString())
+    params.append(queryNames.count, count.toString())
   }
   url += "?" + params.toString()
   let response = await fetch(url, {
