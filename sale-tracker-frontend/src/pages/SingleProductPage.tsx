6cae77edce0cb8dce4ed3fa44b7763c9c2ec7e6d
burakhanunver <burakhanunver@gmail.com>
2024-05-07 14:06:06 +0300

    remove redirect

--- sale-tracker-frontend/src/pages/SingleProductPage.tsx
+++ sale-tracker-frontend/src/pages/SingleProductPage.tsx
@@ -1,11 +1,8 @@
 import SideBar from "@/components/SideBar"
-import { useParams, redirect } from "react-router-dom"
+import { useParams } from "react-router-dom"
 
 export default function SingleProductPage() {
   let { id } = useParams<{ id: string }>()
-  if (!id) {
-    redirect("/products")
-  }
   return (
     <div className="flex min-h-screen">
       <SideBar />
