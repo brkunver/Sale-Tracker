731d218817a109b8c071969fe167817df4eca160
burakhanunver <burakhanunver@gmail.com>
2024-05-08 10:59:43 +0300

    format code

--- sale-tracker-frontend/src/pages/DashboardPage.tsx
+++ sale-tracker-frontend/src/pages/DashboardPage.tsx
@@ -14,7 +14,6 @@ function DashboardPage() {
   async function reloadHandler() {
     reloads.reloadProducts()
     reloads.reloadSales()
-    
   }
 
   return (
@@ -22,9 +21,12 @@ function DashboardPage() {
       <SideBar />
       <main className="flex flex-col items-center text-center mx-auto">
         <h1 className="text-3xl lg:py-8 font-sans">Dashboard</h1>
-        <button onClick={reloadHandler} className="flex justify-center items-center text-lg font-semibold rounded bg-gray-200 px-2 py-1 gap-2 mb-4 hover:outline active:scale-90">
+        <button
+          onClick={reloadHandler}
+          className="flex justify-center items-center text-lg font-semibold rounded bg-gray-200 px-2 py-1 gap-2 mb-4 hover:outline active:scale-90"
+        >
           <span>Reload All</span>
-          <RefreshCcw size={24} className="text-green-700"/>
+          <RefreshCcw size={24} className="text-green-700" />
         </button>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 px-2">
           <Products className="" setRefetch={setReloads} />
