73d7ad923219b61adf878afc2623b40cec03f5c1
burakhanunver <burakhanunver@gmail.com>
2024-05-10 12:02:50 +0300

    add clear inputs after sending new product

--- sale-tracker-frontend/src/components/products/AddProduct.tsx
+++ sale-tracker-frontend/src/components/products/AddProduct.tsx
@@ -11,15 +11,23 @@ export default function AddProduct() {
     },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["products"] })
+      clearInputs()
     },
   })
- 
+
   function formSubmitHandler(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault()
     let formData = new FormData(event.currentTarget)
     mutation.mutate(formData)
   }
- 
+
+  function clearInputs() {
+    const inputs = document.querySelectorAll('input[type="text"], input[type="file"]')
+    inputs.forEach((input) => {
+      ;(input as HTMLInputElement).value = ""
+    })
+  }
+
   return (
     <form onSubmit={formSubmitHandler} className="mx-auto flex flex-col px-8 gap-2 border rounded py-10">
       <h2 className="text-center font-semibold text-lg mb-2">Add New Product</h2>
@@ -31,8 +39,15 @@ export default function AddProduct() {
       <Input type="text" required aria-required name="Name" placeholder="Product Name" />
       <Input type="text" required aria-required name="Description" placeholder="Description" />
 
-      <Input type="text" required aria-required pattern="^\d{1,10}(,\d{1,2})?$" name="Price" placeholder="Price" />
-      
+      <Input
+        type="text"
+        required
+        aria-required
+        pattern="^\d{1,10}(,\d{1,2})?$"
+        name="Price"
+        placeholder="Price, ex : 24,50"
+      />
+
       <Button className="w-full mx-auto px-2" type="submit">
         Add Product
       </Button>
