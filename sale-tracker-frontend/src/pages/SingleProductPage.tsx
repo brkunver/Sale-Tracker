8eb7d79a2021901e9f6bea6c8a4b62f76be43375
burakhanunver <burakhanunver@gmail.com>
2024-05-13 11:08:11 +0300

    created simple logic

--- sale-tracker-frontend/src/pages/SingleProductPage.tsx
+++ sale-tracker-frontend/src/pages/SingleProductPage.tsx
@@ -1,12 +1,25 @@
 import SideBar from "@/components/SideBar"
 import { useParams } from "react-router-dom"
+import { useQuery } from "@tanstack/react-query"
+import { getSingleProduct } from "@/utils/ApiCalls/productApiCalls"
 
 export default function SingleProductPage() {
   let { id } = useParams<{ id: string }>()
-  return (
-    <div className="flex min-h-screen">
-      <SideBar />
-      <div>{id}</div>
-    </div>
-  )
+  const query = useQuery({
+    queryKey: ["single-product", id],
+    queryFn: () => getSingleProduct(id!),
+    enabled: !!id,
+  })
+
+  if (query.isLoading) return <div>Loading...</div>
+  if (query.isError) return <div>Error: </div>
+  if (!query.data) return <div>No Data</div>
+  if (query.isSuccess) {
+    return (
+      <div className="flex min-h-screen">
+        <SideBar />
+        <div>{query.data!.name}</div>
+      </div>
+    )
+  }
 }
