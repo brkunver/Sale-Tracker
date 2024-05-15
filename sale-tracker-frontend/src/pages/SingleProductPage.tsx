64642381e7280b9e9218ab9d7a905c85515817c8
burakhanunver <burakhanunver@gmail.com>
2024-05-15 12:03:14 +0300

    add product info, fix styles

--- sale-tracker-frontend/src/pages/SingleProductPage.tsx
+++ sale-tracker-frontend/src/pages/SingleProductPage.tsx
@@ -11,19 +11,25 @@ export default function SingleProductPage() {
     enabled: !!id,
   })
 
-  if (query.isLoading) return <div>Loading...</div>
-  if (query.isError) return <div>Error: </div>
-  if (!query.data) return <div>No Data</div>
-  if (query.isSuccess) {
-    return (
-      <div className="flex min-h-screen">
-        <SideBar />
-        <main className="mx-auto flex-col flex">
-          <h1>{query.data.name}</h1>
-          <p>{query.data.description}</p>
-          <img src={getImageUrl(query.data.imageUrl)} alt={query.data.name} className="rounded lg:max-w-[400px]" />
-        </main>
-      </div>
-    )
-  }
+  return (
+    <div className="flex min-h-screen">
+      <SideBar />
+      <main className="mx-auto flex flex-col lg:flex-row min-h-screen">
+        {query.isLoading && <div>Loading...</div>}
+        {query.isError && <div>Error</div>}
+        {query.isSuccess && (
+          <section id="product-section" className="grid lg:grid-cols-2 mt-8 lg:gap-4 justify-start items-start h-fit">
+            <img src={getImageUrl(query.data.imageUrl)} alt={query.data.name} className="rounded w-96 object-contain border p-4" />
+            <section id="product-info" className="flex flex-col h-full gap-2">
+              <h1 className="text-xl font-bold">{query.data.name}</h1>
+              <p className="italic">{query.data.description}</p>
+              <p className="font-bold">
+                Price : <span className="font-extrabold text-green-700">{query.data.price} $</span>
+              </p>
+            </section>
+          </section>
+        )}
+      </main>
+    </div>
+  )
 }
