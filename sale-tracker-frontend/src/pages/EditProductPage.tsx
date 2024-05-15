387e4b3db54618e51b7ad3aceed4b7b6958090fc
burakhanunver <burakhanunver@gmail.com>
2024-05-15 11:31:14 +0300

    move loading and error states inside the returned jsx

--- sale-tracker-frontend/src/pages/EditProductPage.tsx
+++ sale-tracker-frontend/src/pages/EditProductPage.tsx
@@ -12,24 +12,21 @@ export default function EditProductPage() {
     enabled: !!id,
   })
 
-  if (query.isLoading) return <div>Loading...</div>
-  if (query.isError) return <div>Error: </div>
-  if (!query.data) return <div>No Data</div>
-  if (query.isSuccess) {
-    return (
-      <div className="flex min-h-screen">
-        <SideBar />
-        <main className="mx-auto flex-col flex items-center">
-          <h1>Edit Product</h1>
-          <h2>{query.data.name}</h2>
-          <img
-            src={getImageUrl(query.data.imageUrl)}
-            alt={query.data.name}
-            className="w-40 rounded object-cover"
-          />
-          <EditProduct product={query.data} />
-        </main>
-      </div>
-    )
-  }
+  return (
+    <div className="flex min-h-screen">
+      <SideBar />
+      <main className="mx-auto flex-col flex items-center">
+        <h1>Edit Product</h1>
+        {query.isLoading && <div>Loading...</div>}
+        {!query.data && <div>No Data</div>}
+        {query.isSuccess && (
+          <div>
+            <h2>{query.data.name}</h2>
+            <img src={getImageUrl(query.data.imageUrl)} alt={query.data.name} className="w-40 rounded object-cover" />
+            <EditProduct product={query.data} />
+          </div>
+        )}
+      </main>
+    </div>
+  )
 }
