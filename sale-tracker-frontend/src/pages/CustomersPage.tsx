722de3d6b020236657398840eddd735012d173b9
burakhanunver <burakhanunver@gmail.com>
2024-05-15 09:50:39 +0300

    change file name

--- sale-tracker-frontend/src/pages/CustomersPage.tsx
+++ sale-tracker-frontend/src/pages/CustomersPage.tsx
@@ -4,7 +4,7 @@ export default function CustomersPage() {
   return (
     <div className="flex min-h-screen">
       <SideBar />
-      <main className="mx-auto flex-col flex">
+      <main>
         <h1>Customers</h1>
       </main>
     </div>
