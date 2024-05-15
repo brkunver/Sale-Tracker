059190d7d593e331285e8b14d92b2ae9ad03359d
burakhanunver <burakhanunver@gmail.com>
2024-05-15 10:22:03 +0300

    create and add edit product page to router

---
+++ sale-tracker-frontend/src/pages/EditProductPage.tsx
@@ -0,0 +1,29 @@
+import SideBar from "@/components/SideBar"
+import { useParams } from "react-router-dom"
+import { useQuery } from "@tanstack/react-query"
+import { getImageUrl, getSingleProduct } from "@/utils/ApiCalls/productApiCalls"
+
+export default function EditProductPage() {
+  let { id } = useParams<{ id: string }>()
+  const query = useQuery({
+    queryKey: ["single-product", id],
+    queryFn: () => getSingleProduct(id!),
+    enabled: !!id,
+  })
+
+  if (query.isLoading) return <div>Loading...</div>
+  if (query.isError) return <div>Error: </div>
+  if (!query.data) return <div>No Data</div>
+  if (query.isSuccess) {
+    return (
+      <div className="flex min-h-screen">
+        <SideBar />
+        <main className="mx-auto flex-col flex">
+          <h1>{query.data.name}</h1>
+          <p>{query.data.description}</p>
+          <img src={getImageUrl(query.data.imageUrl)} alt={query.data.name} className="rounded lg:max-w-[400px]" />
+        </main>
+      </div>
+    )
+  }
+}
