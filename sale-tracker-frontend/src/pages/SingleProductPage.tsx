1d51d7ddc461299fa1a42805486498ad06b2fa55
burakhanunver <burakhanunver@gmail.com>
2024-05-16 16:32:02 +0300

    add delete button to singleproduct

--- sale-tracker-frontend/src/pages/SingleProductPage.tsx
+++ sale-tracker-frontend/src/pages/SingleProductPage.tsx
@@ -3,6 +3,7 @@ import { Link, useParams } from "react-router-dom"
 import { useQuery } from "@tanstack/react-query"
 import { getImageUrl, getSingleProduct } from "@/utils/ApiCalls/productApiCalls"
 import { Button } from "@/components/ui/button"
+import DeleteProduct from "@/components/products/DeleteProduct"
 
 export default function SingleProductPage() {
   let { id } = useParams<{ id: string }>()
@@ -20,7 +21,11 @@ export default function SingleProductPage() {
         {query.isError && <div>Error</div>}
         {query.isSuccess && (
           <section id="product-section" className="grid lg:grid-cols-2 mt-8 lg:gap-4 justify-start items-start h-fit">
-            <img src={getImageUrl(query.data.imageUrl)} alt={query.data.name} className="rounded w-96 object-contain border p-4" />
+            <img
+              src={getImageUrl(query.data.imageUrl)}
+              alt={query.data.name}
+              className="rounded w-96 object-contain border p-4"
+            />
             <section id="product-info" className="flex flex-col h-full gap-2">
               <h1 className="text-xl font-bold">{query.data.name}</h1>
               <p className="italic">{query.data.description}</p>
@@ -30,6 +35,7 @@ export default function SingleProductPage() {
               <Button className="bg-blue-600 hover:bg-blue-800 w-fit text-white">
                 <Link to={`/edit-product/${query.data.id}`}>Edit Product</Link>
               </Button>
+              <DeleteProduct productId={query.data.id} productName={query.data.name} message="Delete" />
             </section>
           </section>
         )}
