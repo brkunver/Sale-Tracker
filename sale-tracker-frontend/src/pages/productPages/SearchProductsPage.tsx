b98ba3f416c58710c98d34feb95849421ded13cb
burakhanunver <burakhanunver@gmail.com>
2024-05-17 11:12:47 +0300

    change title

--- sale-tracker-frontend/src/pages/productPages/SearchProductsPage.tsx
+++ sale-tracker-frontend/src/pages/productPages/SearchProductsPage.tsx
@@ -6,7 +6,7 @@ export default function EditProductPage() {
     <div className="flex min-h-screen">
       <SideBar />
       <main className="mx-auto flex-col flex items-center">
-        <h1 className="font-bold text-3xl my-4">Edit Product</h1>
+        <h1 className="font-bold text-3xl my-4">Search Products</h1>
         <SearchProducts />
       </main>
     </div>
