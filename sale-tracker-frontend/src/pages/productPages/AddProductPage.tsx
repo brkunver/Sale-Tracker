a240bb0ac4d5c5ab879f2ec793f6d34798c31643
burakhanunver <burakhanunver@gmail.com>
2024-05-20 09:55:42 +0300

    create add product page

---
+++ sale-tracker-frontend/src/pages/productPages/AddProductPage.tsx
@@ -0,0 +1,13 @@
+import SideBar from "@/components/SideBar"
+
+export default function AddProductPage() {
+  return (
+    <div className="flex min-h-screen">
+      <SideBar />
+      <main className="mx-auto flex-col flex items-center">
+        <h1 className="font-bold text-3xl my-4">Add Product</h1>
+        
+      </main>
+    </div>
+  )
+}
