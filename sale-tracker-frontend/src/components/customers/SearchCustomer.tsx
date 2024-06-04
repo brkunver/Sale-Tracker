7b2dcfdeb22bbe29219a270506b21e78c6cd005b
burakhanunver <burakhanunver@gmail.com>
2024-06-04 14:20:36 +0300

    create files

--- sale-tracker-frontend/src/components/customers/SearchCustomer.tsx
+++ sale-tracker-frontend/src/components/customers/SearchCustomer.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function SearchCustomer() {
   return (
     <div>SearchCustomer</div>
---
+++ sale-tracker-frontend/src/pages/CustomerPages/SearchCustomerPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SearchCustomerPage() {
+  return (
+    <div>SearchCustomerPage</div>
+  )
+}
+
+export default SearchCustomerPage
