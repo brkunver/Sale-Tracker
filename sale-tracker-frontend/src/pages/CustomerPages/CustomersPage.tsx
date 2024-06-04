ef35fb87013d011ef54eba7dd1aa06ff46fbfaae
burakhanunver <burakhanunver@gmail.com>
2024-06-04 13:35:55 +0300

    fix styles : center customers page

--- sale-tracker-frontend/src/pages/CustomerPages/CustomersPage.tsx
+++ sale-tracker-frontend/src/pages/CustomerPages/CustomersPage.tsx
@@ -1,12 +1,14 @@
 import SideBar from "@/components/SideBar"
 import Customers from "@/components/customers/Customers"
+import { useState } from "react"
 
 export default function CustomersPage() {
+  const [page, setPage] = useState(1)
   return (
     <div className="flex min-h-screen">
       <SideBar />
-      <main>
-        <h1>Customers</h1>
+      <main className="mx-auto">
+        <h1 className="text-center font-bold text-2xl">Customers</h1>
         <Customers showDelete/>
       </main>
     </div>
