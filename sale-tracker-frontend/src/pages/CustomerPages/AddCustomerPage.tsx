1543f08983c6cb57401d7019513d211615029d7e
burakhanunver <burakhanunver@gmail.com>
2024-06-04 14:34:53 +0300

    add basic layout to page

--- sale-tracker-frontend/src/pages/CustomerPages/AddCustomerPage.tsx
+++ sale-tracker-frontend/src/pages/CustomerPages/AddCustomerPage.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import SideBar from "@/components/SideBar"
 
 function AddCustomerPage() {
   return (
-    <div>AddCustomerPage</div>
+    <div className="flex min-h-screen">
+      <SideBar />
+      <main className="flex flex-col lg:flex-row mx-auto items-center">
+        <h1 className="text-center font-bold text-2xl mb-4">Add Customer</h1>
+      </main>
+    </div>
   )
 }
 
--- sale-tracker-frontend/src/pages/CustomerPages/SearchCustomersPage.tsx
+++ sale-tracker-frontend/src/pages/CustomerPages/SearchCustomersPage.tsx
@@ -1,7 +1,14 @@
-import React from "react"
+import SideBar from "@/components/SideBar"
 
 function SearchCustomersPage() {
-  return <div>SearchCustomersPage</div>
+  return (
+    <div className="flex min-h-screen">
+      <SideBar />
+      <main className="flex flex-col lg:flex-row mx-auto items-center">
+        <h1 className="text-center font-bold text-2xl mb-4">Search Customers</h1>
+      </main>
+    </div>
+  )
 }
 
 export default SearchCustomersPage
