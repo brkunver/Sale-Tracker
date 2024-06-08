9358f2a0cbbcaa1b9b7891c0fdf4cec19e8f3ae9
burakhanunver <burakhanunver@gmail.com>
2024-06-08 13:40:11 +0300

    add 'AddCustomer' component to addcustomerpage

--- sale-tracker-frontend/src/pages/CustomerPages/AddCustomerPage.tsx
+++ sale-tracker-frontend/src/pages/CustomerPages/AddCustomerPage.tsx
@@ -1,4 +1,5 @@
 import SideBar from "@/components/SideBar"
+import AddCustomer from "@/components/customers/AddCustomer"
 
 function AddCustomerPage() {
   return (
@@ -6,6 +7,7 @@ function AddCustomerPage() {
       <SideBar />
       <main className="flex flex-col lg:flex-row mx-auto items-center">
         <h1 className="text-center font-bold text-2xl mb-4">Add Customer</h1>
+        <AddCustomer />
       </main>
     </div>
   )
