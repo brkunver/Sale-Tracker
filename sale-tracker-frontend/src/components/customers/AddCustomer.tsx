acd7b17283809dd315753b010c3f14ca4d16e380
burakhanunver <burakhanunver@gmail.com>
2024-06-08 13:39:40 +0300

    add logic to add customer component

--- sale-tracker-frontend/src/components/customers/AddCustomer.tsx
+++ sale-tracker-frontend/src/components/customers/AddCustomer.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import { addNewCustomer } from "../../utils/ApiCalls/customerApiCalls"
+import { Button } from "../ui/button"
+import { Input } from "../ui/input"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { useNavigate } from "react-router-dom"
 
 function AddCustomer() {
+  const navigate = useNavigate()
+  const queryClient = useQueryClient()
+  const mutation = useMutation({
+    mutationFn: async (formData: FormData) => {
+      return await addNewCustomer(formData)
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["customers"] })
+      navigate("/customers")
+    },
+  })
+  function formSubmitHandler(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    let formData = new FormData(event.currentTarget)
+    mutation.mutate(formData)
+  }
   return (
-    <div>AddCustomer</div>
+    <form onSubmit={formSubmitHandler} className="mx-auto flex flex-col px-8 gap-2 border rounded py-10">
+      <h2 className="text-center font-semibold text-lg mb-2">Add New Customer</h2>
+      <Input type="text" required aria-required name="Name" placeholder="Customer Name" maxLength={100} />
+      <Input type="text" required aria-required name="Phone" placeholder="Phone" pattern="\d{6,15}" />
+      <Input type="text" required aria-required name="Address" placeholder="London" maxLength={255} />
+      <Button className="w-full mx-auto px-2" type="submit">
+        Add Customer
+      </Button>
+    </form>
   )
 }
 
