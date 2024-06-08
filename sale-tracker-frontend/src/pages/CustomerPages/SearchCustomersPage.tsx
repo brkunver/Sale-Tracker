29a722013224c1b3a919a11d7b9558857631cc8e
burakhanunver <burakhanunver@gmail.com>
2024-06-08 13:48:59 +0300

    add search customer component to searchcustomerpage

--- sale-tracker-frontend/src/pages/CustomerPages/SearchCustomersPage.tsx
+++ sale-tracker-frontend/src/pages/CustomerPages/SearchCustomersPage.tsx
@@ -1,11 +1,12 @@
 import SideBar from "@/components/SideBar"
-
+import SearchCustomer from "@/components/customers/SearchCustomer"
 function SearchCustomersPage() {
   return (
     <div className="flex min-h-screen">
       <SideBar />
-      <main className="flex flex-col lg:flex-row mx-auto items-center">
+      <main className="flex flex-col mx-auto items-center">
         <h1 className="text-center font-bold text-2xl mb-4">Search Customers</h1>
+        <SearchCustomer />
       </main>
     </div>
   )
