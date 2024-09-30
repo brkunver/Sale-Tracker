2be77537470f5e03af5bff9c5f70f3c12c0f13a8
Burakhan Ünver <43881727+brkunver@users.noreply.github.com>
2024-09-30 15:01:58 +0300

    now api call functions returns mock data if in demo mode

--- sale-tracker-frontend/src/utils/ApiCalls/saleApiCalls.ts
+++ sale-tracker-frontend/src/utils/ApiCalls/saleApiCalls.ts
@@ -1,13 +1,19 @@
 import type { SaleData, SingleSaleData } from "@/types/saleTypes"
-
+import { mockSaleData } from "@/utils/demoBackend/mockSaleData"
 enum queryNames {
   page = "page",
   count = "count",
 }
 
 import getTokenHeader from "./getTokenHeader"
+import isDemo from "../demoBackend/isDemo"
+import getRandomSingleSaleData from "../demoBackend/mockSingleSaleData"
 
-export async function getAllSales(page?: number, count?: number) {
+export async function getAllSales(page?: number, count?: number): Promise<SaleData> {
+  if (isDemo()) {
+    return mockSaleData
+  }
+
   let url = import.meta.env.VITE_API_URL + "/api/sale"
   let params = new URLSearchParams()
   if (page) {
@@ -27,26 +33,36 @@ export async function getAllSales(page?: number, count?: number) {
   return data as SaleData
 }
 
-export async function getSaleById(saleId: string) {
-  let url = import.meta.env.VITE_API_URL + "/api/sale/" + saleId
-  let response = await fetch(url, {
-    headers: getTokenHeader(),
-  })
-  if (!response.ok) {
-    throw new Error(response.statusText)
+export async function getSaleById(saleId: string): Promise<SingleSaleData> {
+  if (isDemo()) {
+    return getRandomSingleSaleData()
+  } else {
+    let url = import.meta.env.VITE_API_URL + "/api/sale/" + saleId
+    let response = await fetch(url, {
+      headers: getTokenHeader(),
+    })
+
+    if (!response.ok) {
+      throw new Error(response.statusText)
+    }
+
+    let data = await response.json()
+    return data as SingleSaleData
   }
-  let data = await response.json()
-  return data as SingleSaleData
 }
 
-export async function getLastSales(day?: number) {
-  let url = import.meta.env.VITE_API_URL + "/api/sale/last-sales?" + "count=" + (day?.toString() ?? "7")
-  let response = await fetch(url, {
-    headers: getTokenHeader(),
-  })
-  if (!response.ok) {
-    throw new Error(response.statusText)
+export async function getLastSales(day?: number): Promise<number[]> {
+  if (isDemo()) {
+    return [1200, 675, 800, 950, 1900]
+  } else {
+    let url = import.meta.env.VITE_API_URL + "/api/sale/last-sales?" + "count=" + (day?.toString() ?? "7")
+    let response = await fetch(url, {
+      headers: getTokenHeader(),
+    })
+    if (!response.ok) {
+      throw new Error(response.statusText)
+    }
+    let data = await response.json()
+    return data.data as number[]
   }
-  let data = await response.json()
-  return data.data as number[]
 }
