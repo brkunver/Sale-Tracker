af7efbca4eee7901bc261334b99ad37ff53094e1
Burakhan Ünver <43881727+brkunver@users.noreply.github.com>
2024-10-24 13:29:44 +0300

    fix error

--- sale-tracker-frontend/src/utils/demoBackend/mockSingleSaleData.ts
+++ sale-tracker-frontend/src/utils/demoBackend/mockSingleSaleData.ts
@@ -1,18 +1,21 @@
-import { Sale, SaleData, SingleSaleData } from "@/types/saleTypes"
+import { Sale, SaleDetail, SingleSaleData } from "@/types/saleTypes"
 
 export default function getRandomSingleSaleData(id: string): SingleSaleData {
-  const saleDetailsData  = [
-      {
-        
-      }
+  const saleDetailsData: SaleDetail[] = [
+    {
+      productId: "prod1",
+      saleId: id,
+      quantity: 1,
+      saledPrice: 1000,
+    },
   ]
 
   const saleData: Sale = {
     customerId: "cust1",
     id: id,
-    total : 1500,
-    saledOn : "2024-05-05",
-    saleDetails : saleDetailsData
+    total: 1500,
+    saledOn: "2024-05-05",
+    saleDetails: saleDetailsData,
   }
 
   const data: SingleSaleData = {
